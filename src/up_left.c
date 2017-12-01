9ef347953f30c8f2b7addf102184b92caba8d0c8
Unknown <bertinelliot6@gmail.com>
2017-12-01 17:29:02 +0100

    PAUL T UN GENI JTM <3

--- src/up_left.c
+++ src/up_left.c
@@ -5,123 +5,55 @@
 #include <unistd.h>
 #include "../includes/fillit.h"
 
-void    ft_puttab(char **map)
+void	up_line(char *str)
 {
-	int    x;
-
-	x = 0;
-	while (x < 4)
+	//temps que aucun des caracteres de la prmiere ligne est un #, on continue
+	while (!ft_strnstr(str, "#", 4))
 	{
-		printf("%s\n", map[x]);
-		x++;
+		//decale les caracteres de la ligne vers la ligne au dessus
+		//str est la destination, str + 4 est l'adresse du prmier caractere de la ligne suivante, 12 c'est le nombre de caracteres
+		//a rehausse
+		ft_memmove(str, str + 4, 12);
+		//la derniere ligne est remplie de .
+		ft_memset(str + 12, '.', 4);
 	}
 }
 
-int        check_up(char **map)
+void	left_col(char *str)
 {
-	int y;
-
-	y = 0;
-	while (y < 4)
+	//temps que la colone de gauche ne contient pas des #, on les bouges a gauche
+	while (str[0] != '#' && str[4] != '#' && str[8] != '#' && str[12] != '#')
 	{
-		if (map[0][y] == '#')
-			return (0);
-		y++;
-	}
-	return (1);
-}
-
-int        check_left(char **map)
-{
-	int x;
-
-	x = 0;
-	while (x < 4)
-	{
-		if (map[x][0] == '#')
-		return (0);
-		x++;
-	}
-	return (1);
-}
-
-void    up(char **map, int y)
-{
-	int temp;
-	int x;
-
-	temp = y + 4;
-	if (!(check_up(map)))
-		return ;
-	while (y < temp)
-	{
-		x = 0;
-		while (x < 4)
-		{
-			if (map[x][y] != '.' && map[x - 1][y] == '.' && x > 0)
-			{
-				map[x][y] = '#';
-				map[x - 1][y] = '.';
-			}
-			x++;
-		}
-		y++;
+		//on decale les trois caracteres de droites de 1 vers la gauche
+		ft_memmove(str, str + 1, 3);
+		str[3] = '.';
+		ft_memmove(str + 4, str + 5, 3);
+		str[7] = '.';
+		ft_memmove(str + 8, str + 9, 3);
+		str[11] = '.';
+		ft_memmove(str + 12, str + 13, 3);
+		str[15] = '.';
 	}
 }
 
-void    left(char **map, int x)
-{
-	int temp;
-	int y;
-
-	temp = x + 4;
-	if (!(check_left(map)))
-		return ;
-	while (x < temp)
-	{
-		y = 0;
-		while (y < 4)
-		{
-			if (map[x][y] != '.' && map[x][y - 1] == '.' && y > 0)
-			{
-				map[x][y] = '#';
-				map[x][y - 1] = '.';
-			}
-			y++;
-		}
-		x++;
-	}
-}
-
-void    up_left(char **map, int count)
+void	up_left(char **tab, int count)
 {
 	int i;
 
 	i = 0;
 	while (i < count)
 	{
-		left(map, i * 4);
-		i++;
-	}
-	i = 0;
-	while (i < count)
-	{
-		up(map, i * 4);
-		i++;
+		up_line(tab[i]);
+		left_col(tab[i++]);
 	}
 }
 
 int main(int argc, char *argv[])
 {
-	char *map[4];
+	char *map[16];
 
-	map[0] = ft_strdup("....");
-	map[1] = ft_strdup("....");
-	map[2] = ft_strdup("..##");
-	map[3] = ft_strdup("..##");
-
+	map[0] = ft_strdup("..........##..##");
 	up_left(map, 1);
-
-	ft_puttab(map);
-	return 0;
+	printf("%s\n", map[0]);
+	return (0);
 }
