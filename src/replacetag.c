f1e7456877d6feab7686a84fce12c6e223b8aab1
Paul CARTAU <pcartau@e3r10p10.42.fr>
2017-12-03 14:08:51 +0100

    fdp

--- src/replacetag.c
+++ src/replacetag.c
@@ -6,7 +6,7 @@
 /*   By: asandolo <asandolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 16:20:14 by asandolo          #+#    #+#             */
-/*   Updated: 2017/12/02 15:30:03 by asandolo         ###   ########.fr       */
+/*   Updated: 2017/12/03 14:08:39 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,15 @@ void			ft_replace_char(char *str, char r, char c)
 void			ft_replace_char_all(char **map)
 {
 	size_t	i;
-	size_t	j;
 	char	c;
 
 	i = 0;
 	c = 'A';
 	while (map[i])
 	{
-		j = 0;
-		while (j < 4)
-		{
-			ft_replace_char(map[i], '#', c);
-			j++;
-			i++;
-		}
+		ft_replace_char(map[i], '#', c);
+		i++;
 		c++;
 	}
+	puttab(map);
 }
