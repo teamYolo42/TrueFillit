3f118eb031621375755381055bf82656c74d6535
Paul CARTAU <pcartau@e3r10p10.42.fr>
2017-12-03 12:22:22 +0100

    fff

--- src/check_valid_pos.c
+++ src/check_valid_pos.c
@@ -3,101 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   check_valid_pos.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcartau <pcartau@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 11:57:35 by pcartau           #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2017/12/02 18:02:33 by ebertin          ###   ########.fr       */
-=======
-/*   Updated: 2017/12/02 18:01:34 by asandolo         ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2017/12/03 11:25:56 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
-<<<<<<< Updated upstream
 #include <stdio.h>
 #include <string.h>
-=======
->>>>>>> Stashed changes
 
-int		check_valid_pos(int cursor, int size, char *tetri, char *map)
+variable	checker(variable var, int cursor)
 {
-	int temp_cursor;
-	int temp_i;
-	int x;
+	while (cursor != var.temp_cursor || var.i != var.temp_i)
+	{
+		if (cursor != var.temp_cursor)
+			cursor++;
+		if (var.i != var.temp_i)
+			var.i++;
+	}
+	return (var);
+}
+
+int init_temp(int cursor, int size)
+{
 	int i;
 
-	i = 0;
-	x = 0;
-	temp_cursor = 0;
-	temp_i = 4;
-	while (tetri[i] && map[cursor])
+	while (cursor != size * size)
 	{
-		while (i != temp_i && cursor != temp_cursor)
+		i = 0;
+		while (i < size)
 		{
-			if (tetri[i] != '.' && map[cursor] == '.')
-				x++;
 			i++;
-			cursor++;
+			if (cursor == ((i * size) - 1))
+				return (cursor);
 		}
-		if (cursor++ == temp_cursor)
-			temp_cursor = cursor + size;
-		else if (i++ == temp_i)
-			temp_i = i + 4;
+		cursor++;
 	}
-	return ((x == 4) ? 1 : 0);
+	return (cursor);
 }
-<<<<<<< Updated upstream
+
+int		check_valid_pos(int curs, int size, char *tetri, char *map)
+{
+	variable var;
+
+	var.i = 0;
+	var.nbdiez = 0;
+	var.cursor = curs;
+	var.temp_cursor = init_temp(var.cursor, size);
+	var.temp_i = 3;
+	while (tetri[var.i] && map[var.cursor])
+	{
+		while (var.i != var.temp_i && var.cursor != var.temp_cursor)
+		{
+			if (tetri[var.i] != '.' && map[var.cursor] == '.')
+				var.nbdiez++;
+			var.i++;
+			var.cursor++;
+		}
+		if (tetri[var.i] != '.' && map[var.cursor] == '.')
+			var.nbdiez++;
+		var = checker(var, var.cursor);
+		var.temp_cursor += size;
+		var.temp_i += 4;
+		var.i++;
+		var.cursor++;
+	}
+	return ((var.nbdiez == 4) ? 1 : 0);
+}
 
 int main(void)
 {
-	printf("%d\n", check_valid_pos(11, 4, strdup("AAAA............"), strdup("................")));
+	printf("%d\n", check_valid_pos(0, 4, strdup("AAAA............"), strdup("................")));
 	return 0;
 }
-
-
-///////////////////
-
-int			check_valid_possub(int pos, int taille_map, char *tetri, char *map)
-{
-	t_var_struct_vp var;
-
-	var.nbdiez = 0;
-	var.linmap = 1;
-	var.i = 0;
-	var.savepos = pos;
-	var.linmapa = var.linmap;
-	//temps que i < 16
-	while (var.i < 16)
-	{
-		//si la position est superieur a la taille de la map, alors c'est faux et on return 0
-		if (pos > (taille_map * taille_map))
-			return (0);
-		//Si la valeur de la position est = a un '.' et que la valeu de la position sur i est un '#'
-		if ((map[pos] == '.' && tetri[var.i] != '.'))
-			//on incremente nbdiez et on check si on a bien atteint les 4 # (On incremente car on sait qu'on
-			//peut placier un '#' correctement)
-			if (++(var.nbdiez) == 4)
-				return (1);
-		// On passe a la valeur suivante dans le tetriminos
-		var.i++;
-		//Si la position de pos (incremente ici pour gagner de la place) % taille map n'est pas egale a 0 (Pk ?)
-		//et que la valeur du tetri est un '#' et que la valeur du tetri % 4 n'est pas 0 (Pk ?) on return 0
-		if (!(++pos % taille_map) && (tetri[var.i] != '.' && (var.i % 4)))
-			return (0);
-		//on sauvegarde la ligne ou on est
-		var.linmapa = var.linmap;
-		//Si la valeur de tetri % 4 n'est pas egale a 0 (Pk ?)
-		if (!(var.i % 4))
-		{
-			// la position devient la valeur de l'ancienne position + la taille de la map * la ligne
-			pos = var.savepos + (taille_map * var.linmap);
-			//on passe a la ligne suivante
-			var.linmap++;
-		}
-	}
-	return (0);
-}
-=======
->>>>>>> Stashed changes
--- src/place_tetri.c
+++ src/place_tetri.c
@@ -3,38 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   place_tetri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcartau <pcartau@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pcartau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/02 15:29:32 by pcartau           #+#    #+#             */
-/*   Updated: 2017/12/02 18:00:05 by asandolo         ###   ########.fr       */
+/*   Updated: 2017/12/03 11:29:35 by pcartau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
+#include <stdio.h>
+#include <string.h>
 
-char	*place_tetri(int cursor, int size, char *tetri, char *map)
+variable	checker(variable var, int cursor)
 {
-	int temp_cursor;
-	int temp_i;
-	int x;
+	while (cursor != var.temp_cursor || var.i != var.temp_i)
+	{
+		if (cursor != var.temp_cursor)
+			cursor++;
+		if (var.i != var.temp_i)
+			var.i++;
+	}
+	return (var);
+}
+
+int init_temp(int cursor, int size)
+{
 	int i;
 
-	i = 0;
-	x = 0;
-	temp_i = 4;
-	while (tetri[i] && map[cursor])
+	while (cursor != size * size)
 	{
-		while (tetri[i] != '\n' && map[cursor] != '\n')
+		i = 0;
+		while (i < size)
 		{
-			if (tetri[i] != '.' && map[cursor] == '.')
-				map[cursor] = tetri[i];
 			i++;
-			cursor++;
+			if (cursor == ((i * size) - 1))
+				return (cursor);
 		}
-		if (cursor++ == temp_cursor)
-			temp_cursor = cursor + size;
-		else if (i == 4)
-			i++;
+		cursor++;
+	}
+	return (cursor);
+}
+
+char	*place_tetri(int curs, int size, char *tetri, char *map)
+{
+	variable var;
+
+	var.i = 0;
+	var.cursor = curs;
+	var.temp_cursor = init_temp(var.cursor, size);
+	var.temp_i = 3;
+	while (tetri[var.i] && map[var.cursor])
+	{
+		while (var.i != var.temp_i && var.cursor != var.temp_cursor)
+		{
+			if (tetri[var.i] != '.' && map[var.cursor] == '.')
+				map[var.cursor] = tetri[var.i];
+			var.i++;
+			var.cursor++;
+		}
+		if (tetri[var.i] != '.' && map[var.cursor] == '.')
+			map[var.cursor] = tetri[var.i];
+		var = checker(var, var.cursor);
+		var.temp_cursor += size;
+		var.temp_i += 4;
+		var.i++;
+		var.cursor++;
 	}
 	return (map);
+}
+
+int main(void)
+{
+	printf("%s\n", place_tetri(8, 4, strdup("AAAA............"), strdup("BB..BB..........")));
+	return 0;
 }
